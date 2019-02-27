aef2d41bce80ddbb4deed99ed75cff4f5f2d5056
haonguyenkhac <47232513+haonguyenkhac@users.noreply.github.com>
2019-02-27 10:26:27 +0700

    Update TodoList.js

--- TodoList.js
+++ TodoList.js
@@ -1,10 +1,32 @@
 $( function() {
-  $(".table").sortable({handle: '.move'});
-});
+  $(".table").sortable({handle: '.move'});});
 
 
 $(document).ready(function(){
-  var item = document.querySelector(".item");
+  var item = document.querySelector(".item"),
+      form = document.querySelector("form");
+  form.addEventListener("submit",function(){
+      if (item.value =="") {
+         alert("You must write something!");
+       }
+       else{
+      $("ul").prepend(
+        $('<li>'
+            +'<span class="move"></span>'
+            +'<input type="checkbox" class="checkbox">'
+            +'<label id="myP" contenteditable="false">'+item.value+'</label>'
+            +'<button class="delete">Delete</button>'
+            +'<button class="ok">Unedit</button>'
+            +'<button class="edit">Edit</button>'
+            +'</li>'));
+        $(".delete").click(function(){ var t=this.parentElement;
+      var h=t.parentNode;
+      h.removeChild(t); store();
+      });
+        store();
+        item.value=""; 
+      }});
+
   $(".add").click(function(){
     if (item.value =="") {
       alert("You must write something!");}
@@ -14,42 +36,35 @@ $(document).ready(function(){
             +'<span class="move"></span>'
             +'<input type="checkbox" class="checkbox">'
             +'<label id="myP" contenteditable="false">'+item.value+'</label>'
-            +'<button class="edit">Edit</button>'
             +'<button class="delete">Delete</button>'
+            +'<button class="ok">Unedit</button>'
+            +'<button class="edit">Edit</button>'
             +'</li>'));
-
-      $('.edit').click(function(){
-        editable(this);store();
-      });
-
-      $(".delete").click(function(){
+      $(".delete").click(function(){ 
         var t=this.parentElement;
         var h=t.parentNode;
         h.removeChild(t); store();});
-
       store();
       item.value=""; 
-      }
-});
-
-      $('.edit').click(function(){
-        editable(this);
-      });
-  
+      }});
       $(".remove").click(function(){
-      $("ul").empty(); remove();
-      });
+      $("ul").empty(); remove();});
+      $(".delete").click(function(){ 
+       var t=this.parentElement;
+       var h=t.parentNode;
+       h.removeChild(t); store();
+       }); 
+     });
 
-      $(".delete").click(function(){
-        var t=this.parentElement;
-        var h=t.parentNode;
-        h.removeChild(t); store();
-      });
-});
-
-function editable(button) {
-  $(button).prev('label').attr('contenteditable','true');
-}
+$(document).on("click", '.edit', function () {
+    $(this).closest("li").find("label").prop("contenteditable", true).focus();
+    return false;
+  });
+$(document).on("click", '.ok', function () {
+    $(this).closest("li").find("label").prop("contenteditable", false).focus();
+    store();
+    return false;
+  });
     
 function store() {
   var table=document.querySelector(".table");
@@ -58,16 +73,13 @@ function store() {
 function remove(){
   if (confirm("Are you sure to remove all task of List???" )){
     table=document.querySelector(".table");
-    localStorage.removeItem("myitems");
-  }
-}
+    localStorage.removeItem("myitems");}}
 
 function getValues() {
   var table=document.querySelector(".table");
   var storedValues = localStorage.myitems;
   if (!storedValues) {
-    "<li>Make a to do list</li>";
-  } 
+    "<li>Make a to do list</li>";} 
   else {
     table.innerHTML = storedValues;
   }
